218dde59dc5d317a26da8f0d711972afb880d86b
gowtham <gowtham.n@rshop.co.in>
2022-06-22 19:39:54 +0530

    Initial commit

---
+++ FIApp.iOS/AppDelegate.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Collections;
+using FIApp.iOS;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+
+
+[assembly: Dependency(typeof(AppDelegate))]
+namespace FIApp.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate, IZelleService
+    {
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this 
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        // You have 17 seconds to return from this method, or iOS will terminate your application.
+        //
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(app, options);
+        }
+
+        public void launchZell(string applicationName, string baseUrl, string institutionId, string ssoKey, string product, IDictionary data)
+        {
+            new UIAlertView("Content", baseUrl, null, "ok", null).Show();
+        }
+    }
+}
